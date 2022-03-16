91ca0309b75f2123ef136e9040ef4b7c6d576d31
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-16 16:25:00 -0700

    wrote create test for sweets table, added app route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Sweet = require('../lib/models/Sweet');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -11,4 +12,17 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('creates a sweet', async () => {
+    const expected = {
+      name: 'Twix',
+      type: 'Chocolate'
+    };
+
+    const res = await request(app).post('/api/v1/sweets').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  
 });
