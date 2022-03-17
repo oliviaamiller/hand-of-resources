2b79f2df62f994823280fd44598d9c262d702e6c
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-17 16:11:08 -0700

    wrote test for gets a list of cats

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Sweet = require('../lib/models/Sweet');
+const Cat = require('../lib/models/Cat');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -39,6 +40,13 @@ describe('hand-of-resources routes', () => {
   it('gets a list of sweets', async () => {
     const expected = await Sweet.getAll();
     const res = await request(app).get('/api/v1/sweets');
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it('gets a list of cats', async () => {
+    const expected = await Cat.getAll();
+    const res = await request(app).get('/api/v1/cats');
 
     expect(res.body).toEqual(expected);
   });
