2bfbb7a68411a8e6d14daa159a332b6ee7937610
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-17 12:29:34 -0700

    added test for getting a list of sweets

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const Sweet = require('../lib/models/Sweet');
+const Sweet = require('../lib/models/Sweet');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -22,6 +22,13 @@ describe('hand-of-resources routes', () => {
     const res = await request(app).post('/api/v1/sweets').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of sweets', async () => {
+    const expected = await Sweet.getAll();
+    const res = await request(app).get('/api/v1/sweets');
+
+    expect(res.body).toEqual(expected);
   });
 
   
