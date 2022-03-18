6671576a70f0d3cdbaad6fcb94dd3b18dc5ac3d5
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-18 09:27:36 -0700

    wrote get list of planets test

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Planet = require('../lib/models/Planet');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -21,6 +22,13 @@ describe('hand-of-resources routes', () => {
     const res = await request(app).post('/api/v1/planets').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of planets', async () => {
+    const expected = await Planet.getAll();
+    const res = await request(app).get('/api/v1/planets');
+
+    expect(res.body).toEqual(expected);
   });
 
 });
