367a010bc4b83d9fdb4b1262309a23f06a4ea738
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-18 11:07:45 -0700

    added get a list of pastas test

--- __tests__/pastas.test.js
+++ __tests__/pastas.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Pasta = require('../lib/models/Pasta');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -22,6 +23,13 @@ describe('hand-of-resources routes', () => {
     const res = await request(app).post('/api/v1/pastas').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of pastas', async () => {
+    const expected = await Pasta.getAll();
+    const res = await request(app).get('/api/v1/pastas');
+
+    expect(res.body).toEqual(expected);
   });
 
 });
